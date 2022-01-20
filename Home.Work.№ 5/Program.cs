c0ea1955eb9a0342a1036c892a29774e9095b72b
Raguf <ragufym@code.edu.az>
2022-01-21 02:10:22 +0400

    Add project files.

---
+++ Home.Work.№ 5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Home.Work.__5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1-50 arasi ancaq 5-e ya da 3-e bolunen ededleri ile alqoritm yazmaq*/
+
+            for (int i = 0; i < 50; i++)
+            {
+                if (i % 3 == 0  || i % 5 == 0)
+
+                {Console.WriteLine(i);
+
+                }
+            }
+            
+        }
+    }
+}
